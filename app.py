cbdb3f1a0ed8080e1a1103322d155943edec3702
ishwik vashishtha <ishwikvashishtha@gmail.com>
2025-01-23 02:40:03 +0530

    completes the login page and profile page now next step is to setup functioning of data base for these two components

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import bson
 import os
 from dotenv import load_dotenv
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 
 # access your MongoDB Atlas cluster
@@ -15,9 +15,17 @@ collection1 = db['users']
 collection2 = db['notes']
 
 users = collection1.find()
+def inser_notes(subject , year,title ,description , file):
+    collection2.insert_one(
+        {
+            "subject": subject,
+            "year": year,
+            "title": title,
+            "description": description,
+            "pdfLink": file
+        }
+    )
 
-
-app = Flask(__name__)
 
 # function to generate the list of all the notes.e
 def list_of_notes():
@@ -26,6 +34,10 @@ def list_of_notes():
     for note in documents:
         Notes.append(note)
     return Notes
+
+
+app = Flask(__name__)
+
 
 # function to filter notes according to the user.
 @app.route('/filter_notes', methods=['GET'])
@@ -52,6 +64,30 @@ def notes():
 @app.route('/resources')
 def resources():
     return render_template("resources.html")
+
+@app.route('/user-profile')
+def userprofile():
+    return render_template("userProfile.html")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # Access form data
+        email = request.form.get('email')
+        password = request.form.get('password')
+        # Here, you can add your authentication logic
+        # For example, validate the credentials against a database
+        if email == 'testmail@gmail.com' and password == 'testpassword':
+            # Successful login logic
+            return redirect(url_for('userprofile' , username= password))
+        else:
+            # Failed login logic
+            error = 'Invalid username or password'
+            return render_template('login.html', error=error)
+    else:
+        # Render the login page
+        return render_template('login.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
